8c3bb83dca0cde14ba1dfb2ff849c0a282807227
isaak gz <yesakgz12@gmail.com>
2023-12-11 02:57:32 -0300

    displaying game score

---
+++ src/Components/CreditScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@chakra-ui/react";
+
+interface CreditScoreProps {
+  score: number;
+}
+
+function CreditScore({ score }: CreditScoreProps) {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+}
+
+export default CreditScore;
--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CreditScore from "./CreditScore";
 
 interface GameCardProps {
   game: Game;
@@ -8,15 +9,18 @@ interface GameCardProps {
 
 function GameCard({ game }: GameCardProps) {
   return (
-    
-      <Card borderRadius={10} overflow={"hidden"}>
-        <Image src={game.background_image} />
-        <CardBody>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
-        </CardBody>
-      </Card>
-    
+    <Card borderRadius={10} overflow={"hidden"}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CreditScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
   );
 }
 
