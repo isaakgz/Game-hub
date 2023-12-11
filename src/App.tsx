d470873db77dd636c2b302e8af0e3cff101fda99
isaak gz <yesakgz12@gmail.com>
2023-12-11 04:45:29 -0300

    fetch the genre

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import GameGrid from "./Components/GameGrid";
+import GenreList from "./Components/GenreList";
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" ></GridItem>
+          <GridItem area="aside" >
+            <GenreList/>
+          </GridItem>
         </Show>
         <GridItem area="main" >
           <GameGrid />
--- src/Components/GameCardContainer.tsx
+++ src/Components/GameCardContainer.tsx
@@ -1,13 +1,22 @@
 import { Box } from "@chakra-ui/react";
-import {  ReactNode } from "react";
+import { ReactNode } from "react";
 interface Props {
   children: ReactNode;
 }
 
-function GameCardContainer({children}:Props) {
-  return <Box width="300px" borderRadius={10} overflow={"hidden"}>
-    { children}
-  </Box>;
+function GameCardContainer({ children }: Props) {
+  return (
+    <Box
+      width={{
+        lg: "300px",
+        xl: "280px",
+      }}
+      borderRadius={10}
+      overflow={"hidden"}
+    >
+      {children}
+    </Box>
+  );
 }
 
 export default GameCardContainer;
