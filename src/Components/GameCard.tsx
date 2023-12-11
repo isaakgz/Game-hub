87642321d57e8928abc997f75872dab661863388
isaak gz <yesakgz12@gmail.com>
2023-12-11 03:19:51 -0300

    get optimized image

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CreditScore from "./CreditScore";
+import getCroppedImageUlr from "../services/imageUrl";
 
 interface GameCardProps {
   game: Game;
@@ -10,7 +11,7 @@ interface GameCardProps {
 function GameCard({ game }: GameCardProps) {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUlr(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
